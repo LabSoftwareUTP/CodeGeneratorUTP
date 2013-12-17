da362c652438c9639d09eefa1aa5c096b6c369f0
Mauricio Aizaga <mauricioaizaga@gmail.com>
2013-12-17 12:05:14 -0500

    Fixed: New way to update and reload server. v3

--- apps/website/views.py
+++ apps/website/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect
 from django.template import RequestContext
 from django.core.urlresolvers import reverse
+from django.http import HttpResponse
 import commands
 import os
 
