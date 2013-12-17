2adec3e4b1e5304073cf678cf4666ad3d6dd63f4
Mauricio Aizaga <mauricioaizaga@gmail.com>
2013-12-17 17:03:57 -0500

    Fixed urls import

--- CodeGenerator/urls.py
+++ CodeGenerator/urls.py
@@ -10,10 +10,10 @@ urlpatterns = patterns('',
     url(r'^$', 'apps.website.views.home', name='home'),
     url(r'^update$', 'apps.website.views.update'),
     url(r'^reload$', 'apps.website.views.reload'),
+    url(r'^code/', include(core_urls)),
     url(r'^account/', include(aurls)),
     url(r'^captcha/', include('captcha.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^eb/', include(eb_urls)),
 )
 
 if settings.DEBUG:
